c6adae4edf3cfb33ad6938ce08c3b518ec4c8aa1
Austriana <austriana@email.com>
2026-07-10 22:59:32 +0200

    added menu

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 let nav = document.getElementById("nav");
+let navContainer = document.getElementById("navContainer");
+let hamburgerContainer = document.getElementById("hamburgerContainer");
+let hamburger = document.getElementById("hamburger");
+
+hamburgerContainer.addEventListener("click", () => {
+    if(navContainer.style.display != "flex"){
+        navContainer.style.display = "flex";
+        hamburger.classList.toggle("active");
+    } else {
+        navContainer.style.display = "none";
+        hamburger.classList.remove("active");
+    }
+});
+
+navContainer.addEventListener("click", (e) => {
+    if(e.target.classList.value === "navlink"){
+        navContainer.style.display = "none";
+        hamburger.classList.remove("active");
+    }
+});
 
 nav.addEventListener("click", (e) => {
     let targetClass = e.target.closest("li").className;
